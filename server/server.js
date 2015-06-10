7c6265fea90a9708e97a06045c0218c11b52b661
Владимир <svr93@i.ua>
2015-06-10 13:26:35 +0400

    server.js___ver2.0

--- server/server.js
+++ server/server.js
@@ -1,15 +1,37 @@
 var http = require('http');
+var nodeStatic = require('node-static');
+var fileServer = new nodeStatic.Server('.');
 
 var PORT = 8000;
 
 http.createServer()
   .on('request', function(req, res) {
-    console.log('Url: ' + req.url);
 
-    res.writeHead(200, "OK", {
-      'Content-Type': 'text/plain'
+    req.on('data', function(chunk) {});
+
+    req.on('end', function() {
+
+      if (req.url == '/upload' && req.method == 'POST') {
+        console.log('Получен файл');
+
+        res.writeHead(200, 'OK');
+        res.end();
+
+        return;
+      }
+
+      if (req.method != 'GET') {
+        res.writeHead(405, 'Method Not Allowed', {
+          'Allow': 'GET'
+        });
+        res.end();
+
+        return;
+      }
+
+      fileServer.serve(req, res);
     });
-    res.end('Success');
+
   })
   .listen(PORT);
 
