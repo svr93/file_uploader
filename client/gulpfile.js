7dac8643e85bffa3518798c0277183a9d5eaff0f
Владимир <svr93@i.ua>
2015-07-01 03:26:08 +0400

    gulpfile.js___ver2.0

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -6,10 +6,20 @@ var replaceHtmlBlocks = require('gulp-html-replace');
 var minifyHtml = require('gulp-html-minifier');
 var checkHtml = require('gulp-w3cjs');
 
+/* ----- css processing ----- */
+
+var translateLess = require('gulp-less');
+
+var LessPluginCleanCSS = require('less-plugin-clean-css');
+var cleanCss = new LessPluginCleanCSS({ advanced: true });
+
+var checkCss = require('gulp-csslint');
+
 /* ----- js processing modules ----- */
 
 var checkJs = require('gulp-jshint');
 var styleOutput = require('jshint-stylish');
+var transformJs = require('gulp-jstransform');
 var minifyJs = require('gulp-closure-compiler-service');
 
 /* ----- other modules ----- */
@@ -27,6 +37,8 @@ gulp.task('html', function() {
 
   gulp.src('index.html')
       .pipe(gulpif(production, replaceHtmlBlocks({
+
+        'css': 'css/main.min.css',
 
         'js': {
           src: 'js/main.min.js',
@@ -47,6 +59,20 @@ gulp.task('html', function() {
 
 });
 
+gulp.task('css', function() {
+
+  gulp.src('less/*.less')
+      .pipe(gulpif(production, translateLess({
+
+        plugins: [cleanCss]
+
+      }), translateLess()))
+      .pipe(checkCss())
+      .pipe(checkCss.reporter())
+      .pipe(gulpif(production, concat('main.min.css')))
+      .pipe(gulp.dest('../server/css'));
+});
+
 gulp.task('js', function() {
 
   gulp.src([
@@ -55,9 +81,15 @@ gulp.task('js', function() {
   ])
       .pipe(checkJs())
       .pipe(checkJs.reporter(styleOutput))
+      .pipe(transformJs({
+
+        harmony: true,
+        target: 'es5'
+
+      }))
       .pipe(gulpif(production, minifyJs()))
       .pipe(gulpif(production, concat('main.min.js')))
       .pipe(gulp.dest('../server/js'));
 });
 
-gulp.task('default', ['html', 'js']);
+gulp.task('default', ['html', 'css', 'js']);
